e51538e81fbc774f7f571a7f8396b83c08c13f1f
Dasha <dashadidyk41@gmail.com>
2024-05-11 11:54:43 +0300

    part2

--- lesson3/main-3.js
+++ lesson3/main-3.js
@@ -1,16 +1,7 @@
 ///- За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
 
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < 10; i++) {
         document.write(
-            '<div>Lorem ipsum dolor sit amet.</div>',
-            '<div>Lorem ipsum dolor sit amet.</div>',
-            '<div>Lorem ipsum dolor sit amet.</div>',
-            '<div>Lorem ipsum dolor sit amet.</div>',
-            '<div>Lorem ipsum dolor sit amet.</div>',
-            '<div>Lorem ipsum dolor sit amet.</div>',
-            '<div>Lorem ipsum dolor sit amet.</div>',
-            '<div>Lorem ipsum dolor sit amet.</div>',
-            '<div>Lorem ipsum dolor sit amet.</div>',
             '<div>Lorem ipsum dolor sit amet.</div>'
         )
 
@@ -18,47 +9,34 @@
 
 ///- За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
 
-    for (let i = 1; i < 10; i++) {
+    for (let i = 1; i <= 10; i++) {
         document.write(
-            `<div>Lorem ipsum dolor sit amet. + ${i++} </div>`,
-            `<div>Lorem ipsum dolor sit amet. + ${i++} </div>`,
-            `<div>Lorem ipsum dolor sit amet. + ${i++} </div>`,
-            `<div>Lorem ipsum dolor sit amet. + ${i++} </div>`,
-            `<div>Lorem ipsum dolor sit amet. + ${i++} </div>`,
-            `<div>Lorem ipsum dolor sit amet. + ${i++} </div>`,
-            `<div>Lorem ipsum dolor sit amet. + ${i++} </div>`,
-            `<div>Lorem ipsum dolor sit amet. + ${i++} </div>`,
-            `<div>Lorem ipsum dolor sit amet. + ${i++} </div>`,
-            `<div>Lorem ipsum dolor sit amet. + ${i++} </div>`,
+            `<div>Lorem ipsum dolor sit amet.  ${i} </div>`
         )
     }
 
 ///- За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
-    let i = 0;
-    while (i < 10){
+    let i = 1;
+    while (i <= 20){
         document.write(
             '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-            '<h1>JavaScript</h1>',
-        )
+        );
+        i++
     }
+
+///- За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині
+    let a = 1;
+     while (a <= 20){
+        document.write(
+            `<h1>JavaScript ${a}</h1>`,
+        );
+        a++
+     }
+
+
+
+
+
 
 
 
